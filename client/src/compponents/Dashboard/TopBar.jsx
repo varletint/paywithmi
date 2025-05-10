0db8d6557332385a46392f2b89c5e8c687ca8291
varletint <intvarlet@gmail.com>
2025-05-10 02:56:56 +0100

    minor

--- client/src/compponents/Dashboard/TopBar.jsx
+++ client/src/compponents/Dashboard/TopBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 export default function TopBar() {
   return (
     <div
       className=' border-b px-4 mb-4 mt-1
   pb-4 border-[#f1f7f3]'>
-      <div className=' flex font-semibold  justify-between items-center'>
+      <div className=' sm:flex block font-semibold  justify-between items-center'>
         <div className=''>
           <span className=' font-semibold text-sm block'>varletint</span>
           <span
@@ -14,10 +15,11 @@ export default function TopBar() {
             Active
           </span>
         </div>
-        <div className=''>
+        <div className='sm:mt-0 mt-2 flex gap-1.5'>
+          <FaExclamationTriangle className=' text-red-500' />
           <span
             className='text-xs  
-      text-red-500'>
+      text-red-500 '>
             For better experience set your device to Desktop view
           </span>
         </div>
