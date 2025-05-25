0333b9d27e31d95ae1a52fb0f04d264786703173
varletint <intvarlet@gmail.com>
2025-05-25 20:20:38 +0100

    refactor: update PrivateRoute component to accept a redirectPath prop

--- client/src/compponents/IsAdminPrivateRoute.jsx
+++ client/src/compponents/IsAdminPrivateRoute.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-export default function PrivateRoute() {
+export default function PrivateRoute({ redirectPath = "/login" }) {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser.isAdmin ? <Outlet /> : <Navigate to='/login' />;
+  if (!currentUser) return <Navigate to={redirectPath} />;
+  return currentUser.isAdmin ? <Outlet /> : <Navigate to={redirectPath} />;
 }
