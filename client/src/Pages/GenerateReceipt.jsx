811aa7a1d54bd3247b019461257c77540f0846b3
varletint <intvarlet@gmail.com>
2025-05-31 06:59:33 +0100

    gR

--- client/src/Pages/GenerateReceipt.jsx
+++ client/src/Pages/GenerateReceipt.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 export default function GenerateReceipt() {
   const referenceSlug = useParams();
   const [fullname, setFullname] = useState("");
-  const [matric_no, setMatricNo] = useState("");
+  const [matric_no, setMatricNo] = useState("u17/fns/css/1129");
   const [paymentData, setPaymentData] = useState("");
   const [error, setError] = useState("");
 
@@ -38,5 +38,59 @@ export default function GenerateReceipt() {
     fetchReferenceDetails();
   }, [referenceSlug.reference]);
 
-  return <div> {referenceSlug.reference}</div>;
+  const handleSubmit = (e) => {};
+
+  return (
+    <div className=' max-w-3xl mx-auto flex justify-center'>
+      <form
+        onSubmit={handleSubmit}
+        className='mt-[8rem] 
+       w-[26rem] px-4 py-2 flex flex-col gap-4'>
+        <div className=' flex justify-between px-0.5 text-[1rem] font-semibold'>
+          <p>pqxoe2321</p>
+          <p
+            className='px-1.5 py-0.5 bg-[#adeec8f3] text-[#038329]
+          rounded font-bold'>
+            <span className=' line-through'>N</span>2300
+          </p>
+        </div>
+        <div className=' flex flex-col gap-1.5'>
+          <label className='px-1'> </label>
+
+          <input
+            type='text'
+            name='fullname'
+            id='name'
+            placeholder='Fullname'
+            className=' border-none bg-[#ddebe0] 
+            placeholder:text-[#8aa197] rounded-[.5rem] px-4 py-2
+          focus:outline-none shadow font-semibold'
+          />
+        </div>
+        <div className=' flex flex-col gap-1.5'>
+          <label className='px-1'> Matric Number</label>
+          <input
+            type='text'
+            name='matric_no'
+            id='matric_no'
+            value={matric_no}
+            className=' border-none bg-slate-200 
+            disabled:text-[#8aa197] rounded-[.5rem] px-4 py-2
+          focus:outline-none shadow disabled:font-semibold
+           disabled:cursor-not-allowed'
+            disabled
+          />
+        </div>
+        <div className=' bg-[#ddebe0] px-3 py-0.5 rounded-xl shadow mt-2'>
+          <select
+            name=''
+            id=''
+            className=' border-none bg-transparent
+          focus:outline-[#8aa197]'>
+            <option value=''></option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
 }
